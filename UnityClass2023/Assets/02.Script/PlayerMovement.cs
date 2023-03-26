0cb212c371131f9f261ac64f75dca18406002694
ahslxjvhapt12 <likjhee1@gmail.com>
2023-03-26 12:27:38 +0900

    .
    .

--- UnityClass2023/Assets/02.Script/PlayerMovement.cs
+++ UnityClass2023/Assets/02.Script/PlayerMovement.cs
@@ -53,7 +53,7 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 target = playerInput.mousePos;
         target.y = 0;
-
+          
         Vector3 v = target - transform.position;
 
         float degree = Mathf.Atan2(v.x, v.z) * Mathf.Rad2Deg;
